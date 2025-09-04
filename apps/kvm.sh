41f6c87b7377bb4501ec2ebeea41b8249649886c
Patrick Boyle <t.patrick.boyle@pm.me>
2025-09-03 21:27:25 -0500

    + replaced hardcoded users with pat variable

--- apps/kvm.sh
+++ apps/kvm.sh
@@ -17,6 +17,6 @@ ensure_kvm_is_installed () {
 
 kvm_is_provisioned () {
   ensure_kvm_is_installed
-  ensure_user_is_in_groups pat libvirt kvm
+  ensure_user_is_in_groups "$USER" libvirt kvm
   ensure_service_is_active libvirtd
 }
